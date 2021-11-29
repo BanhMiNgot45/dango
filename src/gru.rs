66958f5d7aa9d1a75f95ed9552eee291f33894cd
Steven Le <stevenhle459@gmail.com>
2021-11-29 13:42:05 -0600

    Completed backpropagation for GRU cell

--- src/gru.rs
+++ src/gru.rs
@@ -1,7 +1,7 @@
 extern crate ndarray;
 
 use crate::math;
-use ndarray::{Array2};
+use ndarray::{Array2, Axis};
 use std::collections::HashMap;
 
 pub struct GRUCell {
@@ -66,5 +66,46 @@ impl GRUCell {
         self.h_out = self.u.clone() * self.h_in + (Array2::<f32>::ones((self.u.nrows(), self.u.ncols())) - self.u) * self.h_bar;
         self.x_out = self.h_out.dot(params_dict.get("W_v").unwrap().get("value").unwrap()) + params_dict.get("B_v").unwrap().get("value").unwrap();
         (self.h_out, self.x_out)
+    }
+
+    pub fn backward(mut self, x_out_grad: Array2<f32>, h_out_grad: Array2<f32>, mut params_dict: HashMap<String, HashMap<String, Array2<f32>>>) -> (Array2<f32>, Array2<f32>) {
+        let mut temp = params_dict["B_v"]["deriv"].clone() + x_out_grad.sum_axis(Axis(0));
+        params_dict.get("B_v").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        temp = params_dict["W_v"]["deriv"].clone() + self.h_out.t().dot(&x_out_grad);
+        params_dict.get("W_v").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        let mut dh_out = x_out_grad.dot(&params_dict["W_v"]["value"].t());
+        dh_out = dh_out + h_out_grad.clone();
+        let du = self.h_in.clone() * h_out_grad.clone() - self.h_bar * h_out_grad.clone();
+        let dh_bar = (Array2::<f32>::ones((self.u.nrows(), self.u.ncols())) - self.u) * h_out_grad;
+        let dh_bar_int = dh_bar * math::dtanh(self.h_bar_int);
+        temp = params_dict["B_h"]["deriv"].clone() + dh_bar_int.sum_axis(Axis(0));
+        params_dict.get("B_h").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        temp = params_dict["W_xh"]["deriv"].clone() + self.x_in.t().dot(&dh_bar_int);
+        params_dict.get("W_xh").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        let mut dx_in = dh_bar_int.dot(&params_dict.get("W_xh").unwrap().get("value").unwrap().t());
+        temp = params_dict["W_hh"]["deriv"].clone() + self.h_reset.t().dot(&dh_bar_int);
+        params_dict.get("W_hh").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        let dh_reset = dh_bar_int.dot(&params_dict["W_hh"]["value"].t());
+        let dr = dh_reset.clone() * self.h_in.clone();
+        let mut dh_in = dh_reset * self.r;
+        let du_int = math::dsigmoid(self.u_int) * du;
+        temp = params_dict["B_u"]["deriv"].clone() + du_int.sum_axis(Axis(0));
+        params_dict.get("B_u").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        dx_in = dx_in + du_int.dot(&params_dict["W_xu"]["value"].t());
+        temp = params_dict["W_xu"]["deriv"].clone() + self.x_in.t().dot(&du_int);
+        params_dict.get("W_xu").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        dh_in = dh_in + du_int.dot(&params_dict["W_hu"]["value"].t());
+        temp = self.h_in.t().dot(&du_int);
+        params_dict.get("W_hu").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        let dr_int = math::dsigmoid(self.r_int) * dr;
+        temp = params_dict["B_r"]["deriv"].clone() + dr_int.sum_axis(Axis(0));
+        params_dict.get("B_r").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        dx_in = dx_in + dr_int.dot(&params_dict["W_xr"]["value"].t());
+        temp = self.x_in.t().dot(&dr_int);
+        params_dict.get("W_xr").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        dh_in = dh_in + dr_int.dot(&params_dict["W_hr"]["value"].t());
+        temp = self.h_in.t().dot(&dr_int);
+        params_dict.get("W_hr").unwrap().to_owned().insert("deriv".to_owned(), temp);
+        (dx_in, dh_in)
     }
 }
--- src/math.rs
+++ src/math.rs
@@ -9,3 +9,15 @@ pub fn sigmoid(arr: Array2<f32>) -> Array2<f32> {
 pub fn tanh(arr: Array2<f32>) -> Array2<f32> {
     arr.mapv(|a| (f32::powf(std::f32::consts::E, a) - f32::powf(std::f32::consts::E, -a)) / (f32::powf(std::f32::consts::E, a) + f32::powf(std::f32::consts::E, -a)))
 }
+
+pub fn dsigmoid(arr: Array2<f32>) -> Array2<f32> {
+    let temp = sigmoid(arr);
+    let dummy = Array2::<f32>::ones((temp.clone().nrows(), temp.clone().ncols())) - temp.clone();
+    temp * dummy
+}
+
+pub fn dtanh(arr: Array2<f32>) -> Array2<f32> {
+    let temp = tanh(arr);
+    let dummy = temp.mapv(|a| a.powi(2));
+    Array2::<f32>::ones((dummy.nrows(), dummy.ncols())) - dummy
+}

4690c358bcdb77c85b8684e0bc6d546ac42920eb
Steven Le <stevenhle459@gmail.com>
2021-11-28 23:00:12 -0600

    Finished forwards propagation

--- src/gru.rs
+++ src/gru.rs
@@ -15,13 +15,13 @@ pub struct GRUCell {
     h_u: Array2<f32>,
     r_int: Array2<f32>,
     r: Array2<f32>,
-    u_int: f32,
-    u: f32,
+    u_int: Array2<f32>,
+    u: Array2<f32>,
     h_reset: Array2<f32>,
-    x_h: f32,
-    h_h: f32,
-    h_bar_int: f32,
-    h_bar: f32
+    x_h: Array2<f32>,
+    h_h: Array2<f32>,
+    h_bar_int: Array2<f32>,
+    h_bar: Array2<f32>
 }
 
 impl GRUCell {
@@ -37,24 +37,34 @@ impl GRUCell {
             h_u: Array2::zeros((batch_size, hidden_size)),
             r_int: Array2::zeros((batch_size, hidden_size)),
             r: Array2::zeros((batch_size, hidden_size)),
-            u_int: 0.0,
-            u: 0.0,
+            u_int: Array2::zeros((batch_size, hidden_size)),
+            u: Array2::zeros((batch_size, hidden_size)),
             h_reset: Array2::zeros((batch_size, hidden_size)),
-            x_h: 0.0,
-            h_h: 0.0,
-            h_bar_int: 0.0,
-            h_bar: 0.0
+            x_h: Array2::zeros((batch_size, hidden_size)),
+            h_h: Array2::zeros((batch_size, hidden_size)),
+            h_bar_int: Array2::zeros((batch_size, hidden_size)),
+            h_bar: Array2::zeros((batch_size, hidden_size))
         }
     }
 
     pub fn forward(mut self, x_in: Array2<f32>, h_in: Array2<f32>, params_dict: HashMap<String, HashMap<String, Array2<f32>>>) -> (Array2<f32>, Array2<f32>) {
         self.x_in = x_in;
         self.h_in = h_in;
-        self.x_r = x_in.dot(params_dict.get("W_xr").unwrap().get("value").unwrap());
-        self.h_r = h_in.dot(params_dict.get("W_hr").unwrap().get("value").unwrap());
-        self.x_u = x_in.dot(params_dict.get("W_xu").unwrap().get("value").unwrap());
-        self.h_u = h_in.dot(params_dict.get("W_hu").unwrap().get("value").unwrap());
-        self.r_int = self.x_r + self.h_r + params_dict.get("B_r").unwrap().get("value").unwrap();
+        self.x_r = self.x_in.dot(params_dict.get("W_xr").unwrap().get("value").unwrap());
+        self.h_r = self.h_in.dot(params_dict.get("W_hr").unwrap().get("value").unwrap());
+        self.x_u = self.x_in.dot(params_dict.get("W_xu").unwrap().get("value").unwrap());
+        self.h_u = self.h_in.dot(params_dict.get("W_hu").unwrap().get("value").unwrap());
+        self.r_int = self.x_r.clone() + self.h_r.clone() + params_dict.get("B_r").unwrap().get("value").unwrap();
         self.r = math::sigmoid(self.r_int);
+        self.u_int = self.x_r + self.h_r + params_dict.get("B_u").unwrap().get("value").unwrap();
+        self.u = math::sigmoid(self.u_int);
+        self.h_reset = self.r * self.h_in.clone();
+        self.x_h = self.x_in.dot(params_dict.get("W_xh").unwrap().get("value").unwrap());
+        self.h_h = self.h_reset.dot(params_dict.get("W_hh").unwrap().get("value").unwrap());
+        self.h_bar_int = self.x_h + self.h_h + params_dict.get("B_h").unwrap().get("value").unwrap();
+        self.h_bar = math::tanh(self.h_bar_int);
+        self.h_out = self.u.clone() * self.h_in + (Array2::<f32>::ones((self.u.nrows(), self.u.ncols())) - self.u) * self.h_bar;
+        self.x_out = self.h_out.dot(params_dict.get("W_v").unwrap().get("value").unwrap()) + params_dict.get("B_v").unwrap().get("value").unwrap();
+        (self.h_out, self.x_out)
     }
 }
--- src/math.rs
+++ src/math.rs
@@ -5,3 +5,7 @@ use ndarray::Array2;
 pub fn sigmoid(arr: Array2<f32>) -> Array2<f32> {
     arr.mapv(|a| 1.0 / (1.0 + f32::powf(std::f32::consts::E, -a)))
 }
+
+pub fn tanh(arr: Array2<f32>) -> Array2<f32> {
+    arr.mapv(|a| (f32::powf(std::f32::consts::E, a) - f32::powf(std::f32::consts::E, -a)) / (f32::powf(std::f32::consts::E, a) + f32::powf(std::f32::consts::E, -a)))
+}
